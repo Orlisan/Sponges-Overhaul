3c66fed1abd5324e14532131aa84719e58a5998c
Orlisan <sangiovanniorlando12@gmail.com>
2026-05-20 14:37:09 +0200

    Non lo ha fixato

--- src/main/java/com/orlisan/spongesoverhaul/blocks/blockEntities/SpongeBlockEntities.java
+++ src/main/java/com/orlisan/spongesoverhaul/blocks/blockEntities/SpongeBlockEntities.java
@@ -5,7 +5,9 @@ import com.orlisan.spongesoverhaul.blocks.SpongeBlocks;
 import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import org.jetbrains.annotations.NotNull;
 
@@ -14,13 +16,18 @@ public class SpongeBlockEntities {
     public static BlockEntityType<CustomSpongeBlockEntity> CUSTOM_SPONGE_BLOCK_ENTITY;
 
     public static void register() {
+        ResourceKey<BlockEntityType<?>> key = ResourceKey.create(
+                Registries.BLOCK_ENTITY_TYPE,
+                Identifier.fromNamespaceAndPath(SpongesOverhaul.MODID, "custom_sponge_block_entity")
+        );
+
         CUSTOM_SPONGE_BLOCK_ENTITY = Registry.register(
                 BuiltInRegistries.BLOCK_ENTITY_TYPE,
                 Identifier.fromNamespaceAndPath(SpongesOverhaul.MODID, "custom_sponge_block_entity"),
                 FabricBlockEntityTypeBuilder.create(
                         CustomSpongeBlockEntity::new,
                         SpongeBlocks.SPONGE_BLOCK
-                ).build()
+                ).build()  // <-- passa il key qui
         );
     }
 }
